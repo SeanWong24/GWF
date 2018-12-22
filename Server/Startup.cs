1ba36d9e904925d351ce08ec29c5bf9386534309
Sean Wong <seanwong24@live.com>
2018-12-22 12:03:13 -0600

    add cors for server project

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -26,6 +26,7 @@ namespace Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +42,7 @@ namespace Server
             }
 
             // app.UseHttpsRedirection();
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
