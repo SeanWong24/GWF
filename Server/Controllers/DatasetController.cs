6b6e21414829d38449968142600d002aabc3f744
Sean Wong <seanwong24@live.com>
2019-01-02 16:38:59 -0600

    add a GetDetail api for DatasetController in server project

--- Server/Controllers/DatasetController.cs
+++ Server/Controllers/DatasetController.cs
@@ -1,12 +1,11 @@
-using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
+using CsvHelper;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using Server.DBModels;
 using Server.Models;
-using Vis.Utilities;
 
 namespace Server.Controllers
 {
@@ -15,16 +14,41 @@ namespace Server.Controllers
     public class DatasetController : ControllerBase
     {
         private IConfiguration configuration;
+        private IHostingEnvironment hostingEnvironment;
 
-        public DatasetController(IConfiguration configuration)
+        public DatasetController(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
         {
             this.configuration = configuration;
+            this.hostingEnvironment = hostingEnvironment;
         }
 
         [HttpGet]
         public object GetInfo()
         {
             return configuration.GetSection("datasetInfo").Get(typeof(DatasetInfo));
+        }
+
+        [HttpGet("detail")]
+        public object GetDetail(string date, int xMin, int yMin, int xMax, int yMax)
+        {
+            var basePath = hostingEnvironment.WebRootPath;
+            var filePath = Path.Combine(basePath, "csv", date + ".csv");
+
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader))
+            {
+                var records = csv.GetRecords<dynamic>();
+                var rangedRecord = from record in records
+                                   let dict = new Dictionary<string, object>(record)
+                                   let lat = int.Parse(dict["latitude"].ToString())
+                                   where lat >= xMin && lat <= xMax
+                                   let lon = int.Parse(dict["longitude"].ToString())
+                                   where lon >= yMin && lon <= yMax
+                                   // Todo calculate mean
+                                   group record by lat / 10 + " " + lon / 10 into g
+                                   select g.FirstOrDefault();
+                return rangedRecord.ToList();
+            }
         }
     }
 }
