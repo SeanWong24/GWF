1ec951df52e6172468b5ba976cd42ffc45e068ef
Sean Wong <seanwong24@live.com>
2019-01-10 01:35:24 -0600

    implement get data from the server for the parallel coordinates in client project

--- Client/src/app/vis/vis-stats/vis-stats.component.ts
+++ Client/src/app/vis/vis-stats/vis-stats.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 import ParCoords from "parcoord-es";
 import { Http } from '@angular/http';
 import { Globals } from 'src/app/globals';
@@ -14,19 +14,39 @@ import { Globals } from 'src/app/globals';
 })
 export class VisStatsComponent implements OnInit {
 
+  private _pickedDate: string;
+  @Input() private set pickedDate(value:string){
+    this._pickedDate=value;
+    this.generateChart();
+  }
+  private get pickedDate(){
+    return this._pickedDate;
+  }
+
+  private detailData: any[];
+
   constructor(private http: Http) { }
 
-  async ngOnInit() {
-    // Todo replace with real data
-    setTimeout(() => {
-      var chart = ParCoords()("div.stats-main-div")
-      chart.data([
-        { RAINC: 0, RAINNC: 0, GLW: 273.35892, QFX: 0.00002854525, SNOW: 0 },
-        { RAINC: 0, RAINNC: 0, GLW: 273.33548, QFX: 0.000028596769, SNOW: 0 },
-        { RAINC: 0, RAINNC: 0, GLW: 273.30606, QFX: 0.000028685186, SNOW: 0 },
-        { RAINC: 0, RAINNC: 0, GLW: 273.2762, QFX: 0.00002877807, SNOW: 0 },
-        { RAINC: 0, RAINNC: 0, GLW: 273.2467, QFX: 0.000028882061, SNOW: 0 },
-      ])
+  ngOnInit() {
+  }
+
+  private async generateChart(){
+    var params =
+      "?date=" + this.pickedDate +
+      "&xMin=" + 0 +
+      "&yMin=" + 0 +
+      "&xMax=" + 698 +
+      "&yMax=" + 638;
+    const response = await this.http.get(Globals.config.serverEndPoint + "/dataset/detail" + params).toPromise();
+    this.detailData = response.json();
+    for (const datum of this.detailData) {
+      delete datum.time;
+      delete datum.latitude;
+      delete datum.longitude;
+    }
+
+    var chart = ParCoords()("div.stats-main-div")
+      chart.data(this.detailData)
         .margin({
           top: 20,
           left: 20,
@@ -35,7 +55,6 @@ export class VisStatsComponent implements OnInit {
         })
         .render()
         .createAxes();
-    }, 1000);
   }
 
 }
